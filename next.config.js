486e1aca0256c89514f56aad1dfb0e3b73a2d6e5
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-03-29 15:58:25 +0900

    Add type check

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withTypescript = require('@zeit/next-typescript');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+
+module.exports = withTypescript({
+  webpack(config, options) {
+    config.resolve.alias = {
+      '~': './src'
+    };
+
+    // typetyeckする
+    if (options.isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+
+    return config;
+  }
+});
